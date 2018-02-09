976b35dde454db5192b3018a45b52b7af780910b
Zhaowei Ren <rzwdsg@gmail.com>
2018-02-09 07:57:50 -0500

    added multiple word search

--- application/views.py
+++ application/views.py
@@ -4,6 +4,7 @@ from webargs.flaskparser import use_args
 from webargs import fields
 import json
 import os
+import operator
 
 
 @app.route('/')
@@ -25,11 +26,20 @@ with open(file_address, 'r') as f:
 @app.route('/quotes')
 @use_args(quotes_args)
 def get_quotes(args):
-    query = args['query'].lower()
+    query = args['query'].lower().split(' ')
+    query_words = [w for w in query if w]
     quote_hits = []
+    print query_words
     # TODO Spelling isoform finding is required
     for quote in quotes:
-        quote = quote
-        if query in quote['text'].lower():
+        quote_words = quote['text'].lower().split(' ')
+        score = 0
+        for word in query_words:
+            if word in quote_words:
+                score += 1
+        if score:
+            quote['score'] = score / len(query_words)
             quote_hits.append(quote)
+    quote_hits.sort(key=operator.itemgetter('score'), reverse=True)
+
     return Response(json.dumps(quote_hits), status=200)
